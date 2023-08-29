afd1f6c7e9f4f7e53ffa91d32e5ac9a9b8f21d97
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-08-29 19:56:59 +1000

    begin implementing menu bar logic

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -3,11 +3,48 @@ import { store } from '../info.js';
 </script>
 
 <template>
-    <nav class="menu_bar">
+    <nav class="menu-bar">
         <ul>
-            <li>File</li>
-            <li>View</li>
-            <li>Help</li>
+            <li @mouseover="hover_menu" @click="toggle_menu" data-target="menu-file">File</li>
+            <li @mouseover="hover_menu" @click="toggle_menu" data-target="menu-view">View</li>
+            <li @mouseover="hover_menu" @click="toggle_menu" data-target="menu-help">Help</li>
         </ul>
     </nav>
 </template>
+
+<script>
+export default {
+    name: 'MenuBar',
+    data() {
+        return {
+            opened_menu: null
+        };
+    },
+    methods: {
+        toggle_menu(event) {
+            event.target.classList.toggle('active');
+            if(this.opened_menu === null) this.opened_menu = event.target;
+            else this.opened_menu = null;
+        },
+
+        hover_menu(event) {
+            if(this.opened_menu !== null && this.opened_menu !== event.target) {
+                // console.log('different one');
+                this.opened_menu.classList.remove('active');
+                event.target.classList.add('active');
+                this.opened_menu = event.target;
+            }
+        }
+    }
+};
+</script>
+
+<style scoped>
+    /* navbar for mobile */
+    @media screen and (max-width: 825px) {
+        .menu-bar ul li {
+            flex: 1;
+            text-align: center;
+        }
+    }
+</style>
--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -3,7 +3,7 @@ import { store } from '../info.js';
 </script>
 
 <template>
-    <header class="title_bar">
+    <header class="title-bar">
         <h1>pCanvas</h1>
         <button class="mini"><b>?</b></button>
     </header>
