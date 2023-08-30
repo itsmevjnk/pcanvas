6b09bc7a78dac78b548cf0b4ef58c2a074998ad0
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-08-30 20:52:58 +1000

    added title prop to TitleBar, and made view handle everything at this point

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import TitleBar from './components/TitleBar.vue'
 </script>
 
 <template>
-    <TitleBar/>
     <RouterView/>
 </template>
 
