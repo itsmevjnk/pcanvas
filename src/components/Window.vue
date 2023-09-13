6463f5f8afe49b94720b14b321f9c1c8eb45e67b
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-09-13 16:37:46 +1000

    implemented window moving

--- src/components/Window.vue
+++ src/components/Window.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { ref } from 'vue';
+
 const props = defineProps({
     z_index: {
         type: [String, Number],
@@ -10,6 +12,8 @@ const props = defineProps({
     },
     title: String
 });
+
+const window_container = ref(null);
 </script>
 
 <template>
@@ -19,12 +23,12 @@ const props = defineProps({
         'non-blocking': props.blocking.toLowerCase() === 'none',
         'blocking': props.blocking.toLowerCase() === 'blocking' || props.blocking.toLowerCase() === 'blocking-bg',
         'bg-checker-black': props.blocking.toLowerCase() === 'blocking-bg'
-    }">
+    }" ref="window_container">
         <div class="window" :style="{
             'top': this.offset.y + 'px',
             'left': this.offset.x + 'px'
         }">
-            <div class="title-bar">
+            <div class="title-bar" @mousemove="handle_drag">
                 <h1>{{ props.title }}</h1>
             </div>
             <slot></slot>
@@ -46,13 +50,57 @@ export default {
         };
     },
 
+    mounted() {
+        window.addEventListener('mousedown', this.handle_click);
+        window.addEventListener('resize', this.handle_resize);
+    },
 
+    unmounted() {
+        window.removeEventListener('mousedown', this.handle_click);
+        window.removeEventListener('resize', this.handle_resize);
+    },
+
+    methods: {
+        handle_click(event) {
+            if(event.buttons & 1 == 0) return; // we only handle left click events
+            if(event.target === this.$refs.window_container || this.$refs.window_container.contains(event.target)) {
+                // console.log('inside');
+                if(this.blocking === 'none') this.$refs.window_container.classList.remove('inactive');
+            } else {
+                // console.log('outside');
+                if(this.blocking === 'none') this.$refs.window_container.classList.add('inactive');
+            }
+        },
+
+        handle_drag(event) {
+            if(event.buttons & 1) {
+                // left button
+                this.offset.x += event.movementX;
+                this.offset.y += event.movementY;
+            }
+        },
+
+        handle_resize(event) {
+            // console.log(this.offset);
+            let x_absmax = (window.innerWidth + this.$refs.window_container.offsetWidth) / 2;
+            // console.log(x_absmax);
+            if(this.offset.x < -x_absmax) this.offset.x = -0.9 * x_absmax;
+            else if(this.offset.x > x_absmax) this.offset.x = 0.9 * x_absmax;
+            let y_absmax = (window.innerHeight - this.$refs.window_container.offsetHeight) / 2;
+            // console.log(y_absmax);
+            if(this.offset.y < -y_absmax) this.offset.y = -0.9 * y_absmax;
+            else if(this.offset.y > y_absmax) this.offset.y = 0.9 * y_absmax;
+        }
+    }
 };
 </script>
 
 <style scoped>
 .window {
-    width: fit-content;
-    height: fit-content;
+    width: max-content;
+    height: max-content;
+    max-width: 80vw;
+    max-height: 80vh;
+    user-select: none;
 }
 </style>
