f9eafc8d14cf79b2124b52e98aff6d23d31b5f69
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2023-09-16 23:34:13 +1000

    added Google Maps integration

---
+++ src/maps.template.js
@@ -0,0 +1,12 @@
+import { reactive } from 'vue';
+
+export const maps = reactive({
+    api: 'YOUR-API-KEY-GOES-HERE',
+
+    zoom: 15,
+
+    center: {
+        lat: 0.00000000,
+        lng: 0.00000000
+    }
+});
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,9 @@
 import TitleBar from '../components/TitleBar.vue'
 import { RouterLink } from 'vue-router'
 import { disable_ctx_menu_all } from '../utils.js'
+
+import { GoogleMap, Marker } from 'vue3-google-map'
+import { maps } from '../maps.js'
 </script>
 
 <template>
@@ -17,6 +20,14 @@ import { disable_ctx_menu_all } from '../utils.js'
                     GitHub: <a href="https://github.com/itsmevjnk" target="_blank">https://github.com/itsmevjnk</a><br>
                     Email: <a href="mailto:s223145883@deakin.edu.au">s223145883@deakin.edu.au</a>
                 </p>
+                <GoogleMap :api-key="maps.api" :center="maps.center" :zoom="maps.zoom" :style="{
+                    width: '100%',
+                    height: '25vh'
+                }">
+                    <Marker :options="{
+                        position: maps.center
+                    }"/>
+                </GoogleMap>
                 <p>
                     Written in HTML, CSS and JS (JQuery + Vue 3) for the SIT120 2.2C and 3.1D tasks.<br>
                     Backend written in JS (NodeJS + Express) as part of the SIT120 3.1D task.
